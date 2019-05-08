4e5e56658b85dc49552a828d041ada3e16569273
Ethan Gan <egan@mediciventures.com>
2019-05-08 13:54:33 -0500

    Clean up the code

---
+++ SimpsonsCharacterViewer/AppConstant/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  SimpsonsCharacterViewer
+//
+//  Created by 甘忠达 on 5/8/19.
+//  Copyright © 2019 Ethan. All rights reserved.
+//
+
+import Foundation
---
+++ SimpsonsCharacterViewer/Extension/UIViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Extension.swift
+//  SimpsonsCharacterViewer
+//
+//  Created by 甘忠达 on 5/8/19.
+//  Copyright © 2019 Ethan. All rights reserved.
+//
+
+import Foundation
